e35f7276787500366e12ed4fb5311aa508dddc8b
Achim D. Brucker <adbrucker@0x5f.org>
2017-07-23 18:15:59 +0100

    Inital skeleton of a function for parsing rows in the publication table.

--- src/scripts/ScholarScraper.fsx
+++ src/scripts/ScholarScraper.fsx
@@ -32,3 +32,41 @@ let getAuthorPages author = let rec getAuthorPagesRec author n = let page = getA
                                                                  then page::(getAuthorPagesRec author (n+1)) 
                                                                  else [page]
                             getAuthorPagesRec author 0
+(* )
+<tr class="gsc_a_tr">
+  <td class="gsc_a_t">
+    <a href="/citations?view_op=view_citation&amp;hl=en&amp;user=ZWePF1QAAAAJ&amp;citation_for_view=ZWePF1QAAAAJ:WF5omc3nYNoC"
+     class="gsc_a_at">Extending access control models with break-glass</a>
+    <div class="gs_gray">AD Brucker, H Petritsch</div>
+    <div class="gs_gray">Proceedings of the 14th ACM symposium on Access control models and ...<span class="gs_oph">, 2009</span></div>
+  </td>
+  <td class="gsc_a_c">
+    <a href="https://scholar.google.com/scholar?oi=bibs&amp;hl=en&amp;cites=1468743576812343055" class="gsc_a_ac">116</a>
+  </td>
+  <td class="gsc_a_y"><span class="gsc_a_h">2009</span></td>
+</tr>
+*)
+let parsePublicationTd tds =
+                  let parseYear (n:HtmlNode) = try
+                                                 Some(int(n.InnerText()))
+                                               with
+                                                 | _ -> None
+                  let parseCitation (n:HtmlNode) = try
+                                                     int(n.InnerText())
+                                                   with
+                                                    | _ -> 0
+                  let parseCitationId (n:HtmlNode) = try
+                                                       n.Descendants["a"]
+                                                        |> Seq.choose (fun (x:HtmlNode) -> x.TryGetAttribute("href")
+                                                                                         |> Option.map (fun a -> x.InnerText(), a.Value()))
+                                                         |> Seq.head
+                                                         |> snd
+                                                         |> (fun s -> s.Split([| '='; '&' |]))
+                                                         |> Seq.rev
+                                                         |> Seq.head
+                                                         |> Some
+                                                     with
+                                                     | _ -> None
+                  match tds:HtmlNode list with
+                  | [a; citationNode; yearNode]     -> (a.InnerText(), parseCitationId citationNode, parseCitation citationNode, parseYear yearNode)
+                  | _             -> ("", None, 0, None)
