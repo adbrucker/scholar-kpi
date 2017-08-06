a12b21e7ca3b51058c7302145c5282e1daf5cdf3
Achim D. Brucker <adbrucker@0x5f.org>
2017-08-06 14:14:49 +0100

    Moved metrics module into FAKE build and included a simple unit test.

--- tests/LogicalHacking.ScholarKpi.Tests/Tests.fs
+++ tests/LogicalHacking.ScholarKpi.Tests/Tests.fs
@@ -1,12 +1,29 @@
+(* 
+ * This file is part of the scholar-kpi project. 
+ * Copyright (c) 2017 Achim D. Brucker, https://www.brucker.ch
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *)
+
 module LogicalHacking.ScholarKpi.Tests
 
-open LogicalHacking.ScholarKpi
+open LogicalHacking.ScholarKpi.Core.Metrics
 open NUnit.Framework
 
-(*
+
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
+let ``i10-index of empty list is 0`` () =
+  let result = i10Index [] 
   printfn "%i" result
-  Assert.AreEqual(42,result)
-*)
+  Assert.AreEqual(0,result)
+
