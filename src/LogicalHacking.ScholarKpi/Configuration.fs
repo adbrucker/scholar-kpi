44dc4838c39fd34990c39ecd4a340b48ab570ddf
Achim D. Brucker <adbrucker@0x5f.org>
2018-05-20 22:03:07 +0100

    Added SPDX identifier.

--- src/LogicalHacking.ScholarKpi/Configuration.fs
+++ src/LogicalHacking.ScholarKpi/Configuration.fs
@@ -1,6 +1,6 @@
 (*
  * This file is part of the scholar-kpi project.
- * Copyright (c) 2017 Achim D. Brucker, https://www.brucker.ch
+ * Copyright (c) 2017-2018 Achim D. Brucker, https://www.brucker.ch
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,6 +13,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
  *)
 
 namespace LogicalHacking.ScholarKpi.Core
